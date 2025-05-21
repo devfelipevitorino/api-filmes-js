2acf02d8695c30a865402b908825b54953b6411d
devfelipevitorino <devfelipevitorino@gmail.com>
2025-05-21 18:00:13 -0300

    adicionando a busca e corrigindo o footer

--- script.js
+++ script.js
@@ -28,4 +28,52 @@ async function carregarFilmes() {
     }
 }
 
+async function carregarFilmePorNome() {
+    const input = document.querySelector('.input-buscar').value.trim();
+
+    if (input === '') {
+        carregarFilmes();
+    }
+
+    const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=pt-BR&query=${encodeURIComponent(input)}`;
+
+    try {
+        const resposta = await fetch(API_URL); 
+        const dados = await resposta.json();   
+        const filmes = dados.results;          
+
+        filmesContainer.innerHTML = ''; 
+
+        if (filmes.length === 0) {
+            carregarFilmes();
+        }
+
+        filmes.forEach(filme => {
+            const divFilme = document.createElement('div');
+            divFilme.classList.add('filme');
+
+            divFilme.innerHTML = `
+                <img class="capa" src="https://image.tmdb.org/t/p/w500${filme.poster_path}" alt="capa">
+                <h1>${filme.title}</h1>
+                <p>${filme.overview}</p>
+            `;
+
+            filmesContainer.appendChild(divFilme);
+        });
+    } catch (erro) {
+        console.error('Erro ao carregar filmes:', erro);
+    }
+}
+
+const input = document.querySelector('.input-buscar');
+input.addEventListener('keydown', (event) => {
+    if(event.key == 'Enter'){
+        carregarFilmePorNome();
+    }
+})
+
+const lupa = document.querySelector('.lupa-buscar');
+lupa.addEventListener('click', carregarFilmePorNome);
+
+
 carregarFilmes();
